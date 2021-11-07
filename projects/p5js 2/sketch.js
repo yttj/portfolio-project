fd93596c489b631193e9e302c3387915f07b6d7d
yttj <yut704@newschool.edu>
2021-11-08 03:02:55 +0800

    update

--- projects/p5js 2/sketch.js
+++ projects/p5js 2/sketch.js
@@ -1,18 +1,36 @@
+let angle1 = 0;
+let angle2 = 0;
+let scalar = 70;
+
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(710, 400);
   noStroke();
   rectMode(CENTER);
 }
 
 function draw() {
-  background(248,248,255);
+  background(65,105,225);
 
-  let r1 = map(mouseX, 0, width, 0, height);
-  let r2 = height - r1;
+  let ang1 = radians(angle1);
+  let ang2 = radians(angle2);
 
-  fill(210,180,140, r1);
-  rect(width / 2 + r1 / 2, height / 2, r1, r1);
+  let x1 = width / 2 + scalar * cos(ang1);
+  let x2 = width / 2 + scalar * cos(ang2);
 
-  fill(255,228,225, r2);
-  rect(width / 2 - r2 / 2, height / 2, r2, r2);
+  let y1 = height / 2 + scalar * sin(ang1);
+  let y2 = height / 2 + scalar * sin(ang2);
+
+  fill(255,250,240);
+  rect(width * 0.5, height * 0.5, 140, 140);
+
+  fill(216,191,216);
+  ellipse(x1, height * 0.5 - 120, scalar, scalar);
+  ellipse(x2, height * 0.5 + 120, scalar, scalar);
+
+  fill(255, 204, 0);
+  ellipse(width * 0.5 - 120, y1, scalar, scalar);
+  ellipse(width * 0.5 + 120, y2, scalar, scalar);
+
+  angle1 += 2;
+  angle2 += 3;
 }
