f6dceff874146fe4e105c670e84e1d2fa090f72c
yttj <yut704@newschool.edu>
2021-11-08 02:32:26 +0800

    update

---
+++ projects/p5js 3/sketch.js
@@ -0,0 +1,18 @@
+let ball = {};
+let soundFile;
+
+function setup() {
+  createCanvas(710, 100);
+}
+
+function draw() {
+  background(112,128,144);
+  ball.x = constrain(mouseX,0, width);
+  ellipse(ball.x, height / 2, 100, 100);
+}
+
+function mousePressed() {
+  // map the ball's x location to a panning degree
+  // between -1.0 (left) and 1.0 (right)
+  let panning = map(ball.x, 0, width, -1.0, 1.0);
+}
